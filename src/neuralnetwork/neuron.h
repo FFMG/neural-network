2c5a3003614cdc05489560fc97365a9fde885869
FFMG <github@myoddweb.com>
2025-03-30 16:26:26 +0200

    Some code cleanup to prevent inf/-inf

--- src/neuralnetwork/neuron.h
+++ src/neuralnetwork/neuron.h
@@ -26,22 +26,24 @@ public:
   const Neuron& operator=(const Neuron& src);
   virtual ~Neuron();
 
-  void setOutputVal(double val) { _outputVal = val; }
-  double getOutputVal(void) const { return _outputVal; }
+  void set_output_value(double val);
+  double get_output_value() const;
   void forward_feed(const Layer& prevLayer);
-  void calcOutputGradients(double targetVal);
-  void calcHiddenGradients(const Layer& nextLayer);
+  void calculate_output_gradients(double targetVal);
+  void calculate_hidden_gradients(const Layer& nextLayer);
   void updateInputWeights(Layer& prevLayer);
 
 private:
+  
   void Clean();
   static double eta;   // [0.0..1.0] overall net training rate
   static double alpha; // [0.0..n] multiplier of last weight change (momentum)
-  static double randomWeight(void) { return rand() / double(RAND_MAX); }
   double sumDOW(const Layer& nextLayer) const;
+
+  std::vector<double> he_initialization(int num_neurons_prev_layer);
   
   const unsigned _index;
-  double _outputVal;
+  double _output_value;
   double _gradient;
   std::vector<Connection>* _output_weights;
   const activation::method _activation_method;
