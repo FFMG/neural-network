1de60f87de4faddbe792af907e4bf44df73c6712
FFMG <github@myoddweb.com>
2025-04-10 08:07:58 +0200

    Added current error in training as part of the callback + some cleanup

--- src/neuralnetwork/neuralnetwork.h
+++ src/neuralnetwork/neuralnetwork.h
@@ -18,7 +18,7 @@ public:
     const std::vector<std::vector<double>>& training_inputs,
     const std::vector<std::vector<double>>& training_outputs,
     int number_of_epoch,
-    const std::function<void(int)>& progress_callback = nullptr
+    const std::function<void(int, double)>& progress_callback = nullptr
   );
 
   std::vector<std::vector<double>> think(
@@ -30,7 +30,11 @@ public:
 
 private:
   void forward_feed(const std::vector<double>& inputVals, std::vector<Neuron::Layer>& layers_src) const;
-  void back_propagation(const std::vector<double>& targetVals, std::vector<Neuron::Layer>& layers_src) const;
+  double back_propagation(
+    const std::vector<double>& targetVals, 
+    std::vector<Neuron::Layer>& layers_src,
+    const double current_recent_average_error
+  ) const;
 
   std::vector<Neuron::Layer>* _layers;
   const activation::method _activation_method;
