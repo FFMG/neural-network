7f00c448f4db9c4fc3ea7a06236492605482f01c
FFMG <github@myoddweb.com>
2025-04-16 08:08:36 +0200

    We can now save

--- src/neuralnetwork/neuralnetworkserializer.h
+++ src/neuralnetwork/neuralnetworkserializer.h
@@ -18,7 +18,17 @@ public:
   static void save(const NeuralNetwork& nn, const std::string& path);
 
 private:
+  static std::vector<unsigned> get_topology(const TinyJSON::TJValue& json );
+  static activation::method get_activation_method(const TinyJSON::TJValue& json );
+  static Neuron::Layer* get_layer(
+    const TinyJSON::TJValue& json, 
+    unsigned layer_number,
+    const activation::method& activation_method
+  );
+  static std::vector<std::array<double,2>> get_weights(const TinyJSON::TJValueObject& neuron);
+
   static void add_topology(const NeuralNetwork& nn, TinyJSON::TJValueObject& json);
+  static void add_activation_method(const NeuralNetwork& nn, TinyJSON::TJValueObject& json);
   static void add_layers(const NeuralNetwork& nn, TinyJSON::TJValueObject& json);
   static void add_layer(const Neuron::Layer& layer, TinyJSON::TJValueArray& layers);
   static void add_neuron(const Neuron& neuron, TinyJSON::TJValueArray& layer);
