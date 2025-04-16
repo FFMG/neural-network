f19a3c5b698b06ba70e20f7cfb64afb232768676
FFMG <github@myoddweb.com>
2025-04-16 08:20:17 +0200

    Updated document to make it easier to read.

--- src/neuralnetwork/main.cpp
+++ src/neuralnetwork/main.cpp
@@ -8,16 +8,20 @@
 #include <iomanip>
 #include <vector>
 
-int main()
+NeuralNetwork* create_neural_network()
 {
-  const char* file_name = "./file.nn";
+  std::vector<unsigned> topology = {3,2,1};
+  return new NeuralNetwork(topology, activation::sigmoid_activation);
+}
 
+void train_neural_network( NeuralNetwork& nn, unsigned epoch)
+{
   // XOR training input, 3 values in at a time.
   std::vector<std::vector<double>> training_inputs = {
-      {0, 0, 1},
-      {1, 1, 1},
-      {1, 0, 1},
-      {0, 1, 1}
+    {0, 0, 1},
+    {1, 1, 1},
+    {1, 0, 1},
+    {0, 1, 1}
   };
 
   // output, one single value for each input.
@@ -26,14 +30,12 @@ int main()
   };
 
   // the topology is 3 input, 1 output and one hidden layer with 3 neuron
-  auto* nn = new NeuralNetwork({3,2,1}, activation::sigmoid_activation);
-  nn->train(training_inputs, training_outputs, 50000);
-
+  nn.train(training_inputs, training_outputs, epoch);
   std::cout << std::endl;
 
   // pass an array of array to think about
   // the result should be close to the training output.
-  auto outputs = nn->think(training_inputs);
+  auto outputs = nn.think(training_inputs);
   std::cout << "Output After Training:" << std::endl;
   std::cout << std::fixed << std::setprecision(10);
   for (const auto& row : outputs) 
@@ -43,7 +45,39 @@ int main()
       std::cout << val << std::endl;
     }
   }
-  std::cout << std::endl;
+  std::cout << std::endl;  
+}
+
+int main()
+{
+  // the file we will be loading from
+  const char* file_name = "./xor.nn";
+  const unsigned epoch = 10000;
+
+  // assume that it does not ei
+  auto nn = NeuralNetworkSerializer::load(file_name);
+  if( nullptr == nn )
+  {
+    // we need to create it
+    nn = create_neural_network();
+
+    // train it
+    train_neural_network(*nn, epoch);
+
+    // save it
+    NeuralNetworkSerializer::save(*nn, file_name);
+
+    auto nn_saved = NeuralNetworkSerializer::load(file_name);
+    std::cout << "Output from saved file:" << std::endl;
+    std::cout << std::fixed << std::setprecision(10);
+    auto t1 = nn_saved->think({ 0, 0, 1 });
+    std::cout << t1.front() << std::endl;//  should be close to 0
+    auto t2 = nn_saved->think({ 1, 1, 1 });
+    std::cout << t2.front() << std::endl; //  should be close to 1
+
+    delete nn_saved;
+  }
+
   std::cout << "Output After Training:" << std::endl;
   std::cout << std::fixed << std::setprecision(10);
 
@@ -56,9 +90,6 @@ int main()
   // we know that the output only has one value.
   auto t2 = nn->think({ 1, 1, 1 });
   std::cout << t2.front() << std::endl; //  should be close to 1
-
-  // save our neural network
-  NeuralNetworkSerializer::save(*nn, file_name);
 
   delete nn;
   return 0;
