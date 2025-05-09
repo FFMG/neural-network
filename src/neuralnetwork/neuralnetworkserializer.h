1e838dd1ec0de0534bea5224e36db13a486865ee
FFMG <github@myoddweb.com>
2025-05-09 16:18:33 +0200

    Save/load error as well as learning rate from config.

--- src/neuralnetwork/neuralnetworkserializer.h
+++ src/neuralnetwork/neuralnetworkserializer.h
@@ -18,6 +18,8 @@ public:
   static void save(const NeuralNetwork& nn, const std::string& path);
 
 private:
+  static double get_error(const TinyJSON::TJValue& json);
+  static double get_learning_rate(const TinyJSON::TJValue& json);
   static std::vector<unsigned> get_topology(const TinyJSON::TJValue& json );
   static activation::method get_activation_method(const TinyJSON::TJValue& json );
   static Neuron::Layer* get_layer(
@@ -27,7 +29,7 @@ private:
   );
   static std::vector<std::array<double,2>> get_weights(const TinyJSON::TJValueObject& neuron);
 
-  static void add_basic(TinyJSON::TJValueObject& json);
+  static void add_basic(const NeuralNetwork& nn, TinyJSON::TJValueObject& json);
   static void add_topology(const NeuralNetwork& nn, TinyJSON::TJValueObject& json);
   static void add_activation_method(const NeuralNetwork& nn, TinyJSON::TJValueObject& json);
   static void add_error(const NeuralNetwork& nn, TinyJSON::TJValueObject& json);
