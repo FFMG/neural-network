b10a4c73a53e590427482c927287ed4c65d80439
FFMG <github@myoddweb.com>
2025-05-22 09:26:58 +0200

    Added batch checking, batch spliting and batch error checking.

--- src/neuralnetwork/neuralnetwork.h
+++ src/neuralnetwork/neuralnetwork.h
@@ -16,12 +16,7 @@ public:
 
   virtual ~NeuralNetwork();
 
-  void train(
-    const std::vector<std::vector<double>>& training_inputs,
-    const std::vector<std::vector<double>>& training_outputs,
-    int number_of_epoch,
-    const std::function<bool(int, NeuralNetwork&)>& progress_callback = nullptr
-  );
+  void train(const std::vector<std::vector<double>>& training_inputs, const std::vector<std::vector<double>>& training_outputs, int number_of_epoch, int batch_size = -1, const std::function<bool(int, NeuralNetwork&)>& progress_callback = nullptr);
 
   std::vector<std::vector<double>> think(
     const std::vector<std::vector<double>>& inputs
@@ -37,16 +32,17 @@ public:
   double get_learning_rate() const;
 
 private:
+  void train( const std::vector<std::vector<double>>& training_inputs, const std::vector<std::vector<double>>& training_outputs, int number_of_epoch, const std::function<bool(int, NeuralNetwork&)>& progress_callback);
+  void train_in_batch( const std::vector<std::vector<double>>& training_inputs, const std::vector<std::vector<double>>& training_outputs, int number_of_epoch, int batch_size, const std::function<bool(int, NeuralNetwork&)>& progress_callback);
   static std::vector<double> forward_feed(const std::vector<double>& inputs, std::vector<Layer>& layers_src);
   static std::vector<std::vector<double>> forward_feed(const std::vector<std::vector<double>>& inputs_batch, std::vector<Layer>& layers_src);
   
   static void back_propagation( const std::vector<double>& current_output,  std::vector<Layer>& layers_src );
-  static void back_propagation(const std::vector<double>& current_output, std::vector<Layer>& layers_src);
 
   static void calculate_output_gradients(const std::vector<double>& current_output, Layer& output_layer);
 
   // Todo this should be moved to a static class a passed as an object.
-  static double calculate_error(const std::vector<std::vector<double>>& ground_truth, const std::vector<std::vector<double>>& predictions);
+  static double calculate_error(const std::vector<std::vector<double>>& training_inputs, const std::vector<std::vector<double>>& training_outputs, std::vector<Layer>& layers);
 
   // Huber Loss blends MAE and RMSE — it uses squared error when the difference is small (|error| < delta), and absolute error when it’s large.
   static double calculate_huber_loss(const std::vector<std::vector<double>>& ground_truth, const std::vector<std::vector<double>>& predictions, double delta = 1.0);
@@ -56,7 +52,10 @@ private:
   static double calculate_mse_error(const std::vector<std::vector<double>>& ground_truth, const std::vector<std::vector<double>>& predictions);
   static double calculate_rmse_error(const std::vector<std::vector<double>>& ground_truth, const std::vector<std::vector<double>>& predictions );
 
-  static std::vector<size_t> get_suffled_indexes(size_t raw_size);
+  static void create_batch_from_indexes(const std::vector<size_t>& shuffled_indexes, const std::vector<std::vector<double>>& training_inputs, const std::vector<std::vector<double>>& training_outputs, std::vector<std::vector<double>>& shuffled_training_inputs, std::vector<std::vector<double>>& shuffled_training_outputs);
+  static void break_shuffled_indexes(const std::vector<size_t>& shuffled_indexes, std::vector<size_t>& training_indexes, std::vector<size_t>& checking_indexes, std::vector<size_t>& final_check_indexes);
+  static void create_shuffled_indexes(size_t raw_size, std::vector<size_t>& training_indexes, std::vector<size_t>& checking_indexes, std::vector<size_t>& final_check_indexes);
+  static std::vector<size_t> get_shuffled_indexes(size_t raw_size);
   
   long double _error;
   std::vector<Layer>* _layers;
