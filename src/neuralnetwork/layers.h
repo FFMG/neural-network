eea5acefc79977d0411c386ecba47f2973d02c4c
FFMG <github@myoddweb.com>
2025-07-22 06:28:42 +0200

    Added function to calculate residual layer.

--- src/neuralnetwork/layers.h
+++ src/neuralnetwork/layers.h
@@ -17,10 +17,17 @@ public:
   Layers(const Layers& layers);
   Layers(Layers&& layers);
 
+  Layers& operator=(const Layers& layers);
+  Layers& operator=(Layers&& layers);
+  
+  virtual ~Layers() = default;
+
   const std::vector<Layer>& get_layers() const;
   std::vector<Layer>& get_layers();
 
   const Layer& operator[](unsigned index) const;
+  Layer& operator[](unsigned index);
+
   int residual_layer_number(unsigned index) const;
 
   inline size_t size() const {
@@ -28,6 +35,7 @@ public:
   }
 
 private:
+  void add_residual_layer(Layer& layer, const activation::method& activation_method) const;
   int compute_residual_layer(int current_layer_index, int residual_layer_jump) const;
 
   std::vector<Layer> _layers;
