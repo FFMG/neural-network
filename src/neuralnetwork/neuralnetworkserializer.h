d4300b589fd61e2e3aa2a6628c6536c29da961f4
FFMG <github@myoddweb.com>
2025-11-24 17:41:20 +0200

    Fixed serializer

--- src/neuralnetwork/neuralnetworkserializer.h
+++ src/neuralnetwork/neuralnetworkserializer.h
@@ -26,10 +26,11 @@ private:
   static std::map<NeuralNetworkOptions::ErrorCalculation, double> get_errors(const TinyJSON::TJValue& json);
   static double get_mean_absolute_percentage_error(const TinyJSON::TJValue& json);
   static std::vector<WeightParam> get_weight_params(const TinyJSON::TJValueObject& parent);
-  static std::vector<Neuron> get_neurons(const TinyJSON::TJValue& json, unsigned layer_number,const activation::method& activation_method);
-  static std::vector<std::vector<WeightParam>> get_residual_weight_params(const TinyJSON::TJValue& json, unsigned layer_number );
-  static std::vector<int> get_residual_layers(const TinyJSON::TJValue& json);
-  static std::vector<Layer> create_layers(const NeuralNetworkOptions& options, const TinyJSON::TJValue& json, const std::vector<int>& residual_layers);
+  static std::vector<Neuron> get_neurons(const TinyJSON::TJValue& json, unsigned layer_number);
+  static std::vector<std::vector<WeightParam>> get_residual_weights(const TinyJSON::TJValueObject& layer_object, unsigned layer_number);
+  static std::vector<std::vector<WeightParam>> get_weights(const TinyJSON::TJValueObject& layer_object, unsigned layer_number);
+  static std::vector<WeightParam> get_bias_weights(const TinyJSON::TJValueObject& layer_object, unsigned layer_number);
+  static std::vector<Layer> create_layers(const NeuralNetworkOptions& options, const TinyJSON::TJValue& json);
   static const TinyJSON::TJValueObject* get_layer_object(const TinyJSON::TJValue& json, unsigned layer_number);
   static const TinyJSON::TJValueArray* get_layers_array(const TinyJSON::TJValue& json);
   static int get_number_of_layers(const TinyJSON::TJValue& json);
@@ -38,9 +39,13 @@ private:
   static void add_errors(const NeuralNetwork& nn, TinyJSON::TJValueObject& json);
   static void add_layers(const NeuralNetwork& nn, TinyJSON::TJValueObject& json);
   static void add_layer(const Layer& layer, TinyJSON::TJValueArray& layers);
-  static void add_residual_projector(const Layer& layer, TinyJSON::TJValueObject& layer_object);
   static TinyJSON::TJValueObject* add_neuron(const Neuron& neuron);
   static void add_weight_params(const std::vector<WeightParam>& weight_params, TinyJSON::TJValueObject& parent);
+  static TinyJSON::TJValue* add_weight_param(const WeightParam& weight_param);
   static void add_options(const NeuralNetworkOptions& options, TinyJSON::TJValueObject& json);
   static void add_final_learning_rate(const NeuralNetwork& nn, TinyJSON::TJValueObject& json);
+
+  static void add_weights(const Layer& layer, TinyJSON::TJValueObject& layer_object);
+  static void add_bias_weights(const Layer& layer, TinyJSON::TJValueObject& layer_object);
+  static void add_residual_weights(const Layer& layer, TinyJSON::TJValueObject& layer_object);
 };
