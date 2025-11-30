10a06394720548d36383e23f64aa5469a9029c5a
FFMG <github@myoddweb.com>
2025-11-30 22:03:16 +0200

    Removed some dead code from Neuron

--- src/neuralnetwork/neuron.h
+++ src/neuralnetwork/neuron.h
@@ -6,16 +6,12 @@
     #define VALIDATE_DATA 0
   #endif
 #endif
-#include "activation.h"
 #include "layer.h"
 #include "hiddenstate.h"
-#include "optimiser.h"
 #include "weightparam.h"
 #include "./libraries/instrumentor.h"
 
 #include <vector>
-
-#define LEARNING_ALPHA double(0.5)    // momentum, multiplier of last gradient, [0.0..1.0]
 
 class Layer;
 class Neuron
@@ -24,14 +20,12 @@ public:
   enum class Type
   {
     Normal,
-    Dropout,
+    Dropout
   };
 
 public:
   Neuron(
     unsigned index, 
-    const activation& activation,
-    const OptimiserType& optimiser_type,
     const Type& type,
     const double dropout_rate
     );
@@ -41,8 +35,6 @@ public:
     unsigned num_neurons_current_layer,
     unsigned num_neurons_next_layer,
     unsigned index, 
-    const activation& activation,
-    const OptimiserType& optimiser_type,
     const Type& type,
     const double dropout_rate
     );
@@ -56,22 +48,17 @@ public:
 
   unsigned get_index() const;
 
-  const OptimiserType& get_optimiser_type() const noexcept;
   const Type& get_type() const noexcept;
   bool is_dropout() const noexcept;
   double get_dropout_rate() const noexcept;
 
-  inline const activation& get_activation_method() const noexcept { return _activation_method; }
   bool must_randomly_drop() const;
 
 private:
 
   void Clean();
   unsigned _index;
-  activation _activation_method;
-  OptimiserType _optimiser_type;
   
-  const double _alpha; // [0.0..n] multiplier of last weight change (momentum)
   Type _type;
   double _dropout_rate;
 };
