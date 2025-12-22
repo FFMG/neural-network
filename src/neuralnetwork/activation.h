5ea1db282887f95fd1081e7057ceba49e6966b44
FFMG <github@myoddweb.com>
2025-12-22 08:21:23 +0200

    Small performance improvements in Activation

--- src/neuralnetwork/activation.h
+++ src/neuralnetwork/activation.h
@@ -22,6 +22,8 @@ public:
     elu
   };
 
+  using activation_function = double (*)(double, double);
+
   activation(const method method, double alpha = 0.01) noexcept;
   activation(const activation& src) noexcept;
   activation(activation&& src) noexcept;
@@ -29,8 +31,9 @@ public:
   activation& operator=(activation&& src) noexcept;
   ~activation() = default;
 
-  double activate(double x) const;
-  double activate_derivative(double x) const ;
+  inline double activate(double x) const { return _activate_ptr(x, _alpha); }
+  inline double activate_derivative(double x) const { return _derivative_ptr(x, _alpha); }
+
   double momentum() const;
 
   std::vector<double> weight_initialization(int num_neurons_prev_layer, int num_neurons_current_layer) const;
@@ -52,29 +55,31 @@ private:
   static std::vector<double> lecun_initialization(int fan_in, int fan_out) noexcept;
   static std::vector<double> selu_initialization(int fan_in, int fan_out) noexcept;
 
-  static double calculate_selu(double x) noexcept;
-  static double calculate_selu_derivative(double x) noexcept;
-  static double calculate_sigmoid(double x) noexcept;
-  static double calculate_sigmoid_derivative(double x) noexcept;
-  static double calculate_tanh(double x) noexcept;
-  static double calculate_tanh_derivative(double x) noexcept;
-  static double calculate_relu(double x) noexcept;
-  static double calculate_relu_derivative(double x) noexcept;
+  static double calculate_selu(double x, double alpha) noexcept;
+  static double calculate_selu_derivative(double x, double alpha) noexcept;
+  static double calculate_sigmoid(double x, double alpha) noexcept;
+  static double calculate_sigmoid_derivative(double x, double alpha) noexcept;
+  static double calculate_tanh(double x, double alpha) noexcept;
+  static double calculate_tanh_derivative(double x, double alpha) noexcept;
+  static double calculate_relu(double x, double alpha) noexcept;
+  static double calculate_relu_derivative(double x, double alpha) noexcept;
   static double calculate_leakyRelu(double x, double alpha) noexcept;
   static double calculate_leakyRelu_derivative(double x, double alpha) noexcept;
   static double calculate_PReLU(double x, double alpha) noexcept;
   static double calculate_PReLU_derivative(double x, double alpha) noexcept;
-  static double calculate_linear(double x) noexcept;
-  static double calculate_linear_derivative(double x) noexcept;
-  static double calculate_swish(double x) noexcept;
-  static double calculate_swish_derivative(double x) noexcept;
-  static double calculate_mish(double x) noexcept;
-  static double calculate_mish_derivative(double x) noexcept;
-  static double calculate_gelu(double x) noexcept;
-  static double calculate_gelu_derivative(double x) noexcept;
+  static double calculate_linear(double x, double alpha) noexcept;
+  static double calculate_linear_derivative(double x, double alpha) noexcept;
+  static double calculate_swish(double x, double alpha) noexcept;
+  static double calculate_swish_derivative(double x, double alpha) noexcept;
+  static double calculate_mish(double x, double alpha) noexcept;
+  static double calculate_mish_derivative(double x, double alpha) noexcept;
+  static double calculate_gelu(double x, double alpha) noexcept;
+  static double calculate_gelu_derivative(double x, double alpha) noexcept;
   static double calculate_elu(double x, double alpha) noexcept;
   static double calculate_elu_derivative(double x, double alpha) noexcept;
 
   method _method;
   double _alpha;
+  activation_function _activate_ptr;
+  activation_function _derivative_ptr;
 };
