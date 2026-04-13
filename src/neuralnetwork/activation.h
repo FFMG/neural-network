52b4c98d09c13e8dd9ea1f6991d491cc3bce5a07
FFMG <github@myoddweb.com>
2026-04-14 00:59:44 +0200

    Added [[nodiscard]]

--- src/neuralnetwork/activation.h
+++ src/neuralnetwork/activation.h
@@ -48,12 +48,12 @@ public:
 
   [[nodiscard]] double momentum() const noexcept;
 
-  std::vector<double> weight_initialization(int num_neurons_prev_layer, int num_neurons_current_layer) const;
-  double weight_initialization() const;
+  [[nodiscard]] std::vector<double> weight_initialization(int num_neurons_prev_layer, int num_neurons_current_layer) const;
+  [[nodiscard]] double weight_initialization() const;
 
-  std::string method_to_string() const;
-  static std::string method_to_string(method m);
-  static method string_to_method(const std::string& str);
+  [[nodiscard]] std::string method_to_string() const;
+  [[nodiscard]] static std::string method_to_string(method m);
+  [[nodiscard]] static method string_to_method(const std::string& str);
   
   [[nodiscard]] inline method get_method() const noexcept
   { 
@@ -68,36 +68,36 @@ public:
   }
 
 private:
-  static std::vector<double> he_initialization(int fan_in, int fan_out) noexcept;
-  static std::vector<double> xavier_initialization(int fan_in, int fan_out) noexcept;
-  static std::vector<double> lecun_initialization(int fan_in, int fan_out) noexcept;
-  static std::vector<double> selu_initialization(int fan_in, int fan_out) noexcept;
+  [[nodiscard]] static std::vector<double> he_initialization(int fan_in, int fan_out) noexcept;
+  [[nodiscard]] static std::vector<double> xavier_initialization(int fan_in, int fan_out) noexcept;
+  [[nodiscard]] static std::vector<double> lecun_initialization(int fan_in, int fan_out) noexcept;
+  [[nodiscard]] static std::vector<double> selu_initialization(int fan_in, int fan_out) noexcept;
 
-  static double calculate_selu(double x, double alpha) noexcept;
-  static double calculate_selu_derivative(double x, double alpha) noexcept;
-  static double calculate_sigmoid(double x, double alpha) noexcept;
-  static double calculate_sigmoid_derivative(double x, double alpha) noexcept;
-  static double calculate_tanh(double x, double alpha) noexcept;
-  static double calculate_tanh_derivative(double x, double alpha) noexcept;
-  static double calculate_relu(double x, double alpha) noexcept;
-  static double calculate_relu_derivative(double x, double alpha) noexcept;
-  static double calculate_leakyRelu(double x, double alpha) noexcept;
-  static double calculate_leakyRelu_derivative(double x, double alpha) noexcept;
-  static double calculate_PReLU(double x, double alpha) noexcept;
-  static double calculate_PReLU_derivative(double x, double alpha) noexcept;
-  static double calculate_linear(double x, double alpha) noexcept;
-  static double calculate_linear_derivative(double x, double alpha) noexcept;
-  static double calculate_swish(double x, double alpha) noexcept;
-  static double calculate_swish_derivative(double x, double alpha) noexcept;
-  static double calculate_mish(double x, double alpha) noexcept;
-  static double calculate_mish_derivative(double x, double alpha) noexcept;
-  static double calculate_gelu(double x, double alpha) noexcept;
-  static double calculate_gelu_derivative(double x, double alpha) noexcept;
-  static double calculate_elu(double x, double alpha) noexcept;
-  static double calculate_elu_derivative(double x, double alpha) noexcept;
-  static void calculate_softmax(double* begin, double* end) noexcept;
-  static double calculate_softmax(double x, double alpha) noexcept;
-  static double calculate_softmax_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_selu(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_selu_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_sigmoid(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_sigmoid_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_tanh(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_tanh_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_relu(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_relu_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_leakyRelu(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_leakyRelu_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_PReLU(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_PReLU_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_linear(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_linear_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_swish(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_swish_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_mish(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_mish_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_gelu(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_gelu_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_elu(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_elu_derivative(double x, double alpha) noexcept;
+  [[nodiscard]] static void calculate_softmax(double* begin, double* end) noexcept;
+  [[nodiscard]] static double calculate_softmax(double x, double alpha) noexcept;
+  [[nodiscard]] static double calculate_softmax_derivative(double x, double alpha) noexcept;
 
   method _method;
   double _alpha;
