7a8c934215e06e03ac9b657972c10e38bed0e866
FFMG <github@myoddweb.com>
2026-04-15 20:52:57 +0200

    removed unused include

--- src/neuralnetwork/taskqueue.h
+++ src/neuralnetwork/taskqueue.h
@@ -2,14 +2,13 @@
 #include <atomic>
 #include <condition_variable>
 #include <functional>
-#include <iostream>
 #include <mutex>
 #include <queue>
 #include <thread>
 #include <vector>
 
-#include "logger.h"
 #include "./libraries/instrumentor.h"
+#include "logger.h"
 
 template <typename R>
 class TaskQueue
