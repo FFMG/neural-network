4b2c3561dd4ef09c0c062cdbbb5b77871d2e379d
FFMG <github@myoddweb.com>
2026-04-29 15:19:28 +0200

    Added Neuron tests

---
+++ tests/neuron_tests.cpp
@@ -0,0 +1,70 @@
+#include <gtest/gtest.h>
+#include "neuron.h"
+#include <vector>
+
+TEST(NeuronTest, NormalNeuronInitialization)
+{
+  Neuron n(42, Neuron::Type::Normal, 0.0);
+  EXPECT_EQ(n.get_index(), 42);
+  EXPECT_EQ(n.get_type(), Neuron::Type::Normal);
+  EXPECT_FALSE(n.is_dropout());
+}
+
+TEST(NeuronTest, DropoutNeuronInitialization)
+{
+  Neuron n(7, Neuron::Type::Dropout, 0.25);
+  EXPECT_EQ(n.get_index(), 7);
+  EXPECT_EQ(n.get_type(), Neuron::Type::Dropout);
+  EXPECT_TRUE(n.is_dropout());
+  EXPECT_DOUBLE_EQ(n.get_dropout_rate(), 0.25);
+}
+
+TEST(NeuronTest, CopySemantics)
+{
+  Neuron n1(10, Neuron::Type::Dropout, 0.3);
+  Neuron n2 = n1;
+
+  EXPECT_EQ(n2.get_index(), 10);
+  EXPECT_EQ(n2.get_type(), Neuron::Type::Dropout);
+  EXPECT_DOUBLE_EQ(n2.get_dropout_rate(), 0.3);
+}
+
+TEST(NeuronTest, MoveSemantics)
+{
+  Neuron n1(10, Neuron::Type::Dropout, 0.3);
+  Neuron n2 = std::move(n1);
+
+  EXPECT_EQ(n2.get_index(), 10);
+  EXPECT_EQ(n2.get_type(), Neuron::Type::Dropout);
+  EXPECT_DOUBLE_EQ(n2.get_dropout_rate(), 0.3);
+
+  // n1 is reset
+  EXPECT_EQ(n1.get_index(), 0);
+  EXPECT_EQ(n1.get_type(), Neuron::Type::Normal);
+}
+
+TEST(NeuronTest, DropoutBehaviorBoundaries)
+{
+  // 0% dropout should never drop
+  Neuron n_never(1, Neuron::Type::Dropout, 0.0);
+  for(int i=0; i<100; ++i) {
+    EXPECT_FALSE(n_never.must_randomly_drop());
+  }
+
+  // 100% dropout should always drop
+  Neuron n_always(2, Neuron::Type::Dropout, 1.0);
+  for(int i=0; i<100; ++i) {
+    EXPECT_TRUE(n_always.must_randomly_drop());
+  }
+}
+
+#if VALIDATE_DATA == 1
+TEST(NeuronTest, ValidationLogic)
+{
+  Neuron n(1, Neuron::Type::Normal, 0.0);
+  
+  // These should panic for Normal neurons
+  EXPECT_THROW(n.get_dropout_rate(), std::runtime_error);
+  EXPECT_THROW(n.must_randomly_drop(), std::runtime_error);
+}
+#endif
