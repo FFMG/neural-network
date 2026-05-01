f9865fd44af0906cfe5ce00ff065c4f929c6bbc8
FFMG <github@myoddweb.com>
2026-05-01 09:33:19 +0200

    Removed logger from tests/ root class.

--- tests/neuralnetwork.cpp
+++ tests/neuralnetwork.cpp
@@ -1,8 +1,6 @@
 #include <gtest/gtest.h>
-#include "..\..\src\neuralnetwork\logger.h"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    Logger::set_level(Logger::LogLevel::Information);
     return RUN_ALL_TESTS();
 }
