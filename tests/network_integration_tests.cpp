470ecb15c0de4fa86461c3cba1858b2f070b72b1
FFMG <github@myoddweb.com>
2026-06-21 14:28:04 +0200

    Optimise BPTT sequence batching inside NeuralNetwork training loop and add integration tests

--- tests/network_integration_tests.cpp
+++ tests/network_integration_tests.cpp
@@ -427,3 +427,38 @@ TEST(NetworkIntegrationTest, LogTrainingInfoOptionAndSerialization)
   std::remove(test_path.c_str());
 }
 
+TEST(NetworkIntegrationTest, ShuffleBpttBatchesBehavior)
+{
+  auto options_no_shuffle = NeuralNetworkOptions::create({ 2, 2, 1 })
+    .with_learning_rate(0.01)
+    .with_number_of_epoch(5)
+    .with_shuffle_bptt_batches(false)
+    .with_enable_bptt(true)
+    .with_bptt_max_ticks(3)
+    .build();
+
+  auto options_shuffle = NeuralNetworkOptions::create({ 2, 2, 1 })
+    .with_learning_rate(0.01)
+    .with_number_of_epoch(5)
+    .with_shuffle_bptt_batches(true)
+    .with_enable_bptt(true)
+    .with_bptt_max_ticks(3)
+    .build();
+
+  std::vector<std::vector<double>> inputs = {
+    {0.1, 0.2}, {0.3, 0.4}, {0.5, 0.6}, {0.7, 0.8}, {0.9, 1.0}
+  };
+  std::vector<std::vector<double>> outputs = {
+    {0.3}, {0.7}, {1.1}, {1.5}, {1.9}
+  };
+
+  NeuralNetwork nn_no_shuffle(options_no_shuffle);
+  nn_no_shuffle.train(inputs, outputs);
+
+  NeuralNetwork nn_shuffle(options_shuffle);
+  nn_shuffle.train(inputs, outputs);
+
+  SUCCEED();
+}
+
+
