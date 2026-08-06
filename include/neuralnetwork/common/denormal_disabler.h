5388800cb166a051006e2195ca5de0ed91266ce4
Florent <florentg@FFMG-ASUS.local>
2026-08-06 06:33:26 +0200

    Optimize DenormalDisabler by bypassing LDMXCSR CPU pipeline serialization when FTZ/DAZ are already active

--- include/neuralnetwork/common/denormal_disabler.h
+++ include/neuralnetwork/common/denormal_disabler.h
@@ -14,17 +14,21 @@ public:
   {
 #if defined(_MSC_VER) || defined(__x86_64__) || defined(__i386__)
     _old_mxcsr = _mm_getcsr();
-    // Enable FTZ (Flush to Zero) and DAZ (Denormals are Zero)
-    // Bit 15 is FTZ (Flush to Zero)
-    // Bit 6 is DAZ (Denormals are Zero)
-    _mm_setcsr(_old_mxcsr | 0x8000 | 0x0040);
+    _needs_restore = ((_old_mxcsr & 0x8040) != 0x8040);
+    if (_needs_restore)
+    {
+      _mm_setcsr(_old_mxcsr | 0x8000 | 0x0040);
+    }
 #endif
   }
 
   ~DenormalDisabler() noexcept
   {
 #if defined(_MSC_VER) || defined(__x86_64__) || defined(__i386__)
-    _mm_setcsr(_old_mxcsr);
+    if (_needs_restore)
+    {
+      _mm_setcsr(_old_mxcsr);
+    }
 #endif
   }
 
@@ -35,7 +39,8 @@ public:
 
 private:
 #if defined(_MSC_VER) || defined(__x86_64__) || defined(__i386__)
-  unsigned int _old_mxcsr;
+  unsigned int _old_mxcsr{ 0 };
+  bool _needs_restore{ false };
 #endif
 };
 } // namespace myoddweb::nn
